dd8a4fbb349d5ce288467f49f60b391b801d6e34
SinHuanChuk <magicdropovich@gmail.com>
2020-11-18 22:23:03 +0300

    Updates

---
+++ precache-manifest.e70c023e7f59404cad0a8ff5fdef9bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3bd48248bf1aa1ec003917a19a718e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "9c4bf99f77be4a58690a",
+    "url": "/goit-react-hw-01-components/static/css/main.9349369f.chunk.css"
+  },
+  {
+    "revision": "24382d7bac13e1ed8f77",
+    "url": "/goit-react-hw-01-components/static/js/2.c9cc2869.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/goit-react-hw-01-components/static/js/2.c9cc2869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c4bf99f77be4a58690a",
+    "url": "/goit-react-hw-01-components/static/js/main.d212031c.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
